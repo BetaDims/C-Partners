821798c2690d3d1384fe45fc3ea6c7f5524bf94a
BetaDims <45671028+BetaDims@users.noreply.github.com>
2022-07-03 09:46:26 -0500

    First commit

---
+++ Week1/dict.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "key": "value",
+    "key2": "2nd_value",
+    "key3": "3rd_value",
+}
+print(my_dict)
+
+
+# How to acess to my dict
+
+for key,value in my_dict.items():
+    print(key, value)
