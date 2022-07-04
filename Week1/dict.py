3393fea28672351cc1ccbf7758d27bb96a90071f
BetaDims <45671028+BetaDims@users.noreply.github.com>
2022-07-03 19:27:45 -0500

    last commit of the day

--- Week1/dict.py
+++ Week1/dict.py
@@ -10,3 +10,26 @@ print(my_dict)
 
 for key,value in my_dict.items():
     print(key, value)
+
+
+# Let's talk about lists
+
+my_list = [1, 2, 3, 4]
+for new_values in range(5,10):
+    my_list.append(new_values)
+    print(my_list)
+
+# Storing data Data structures & Algorithms example
+
+paths = {
+    "Home": ["Store A", "Store B", "Intersection"],
+    "Store A": ["Home", "Store B"],
+    "Store B": ["School"],
+    "Intersection": ["School"],
+}
+print(paths)
+
+list_paths = [["Home", "Store A"], ["Store A", "Home"], ["Home", "Store B"]]
+
+for key, value in paths.items():
+    print(key, value)
